e5538cdc53203afc3a5b5d598ff5bedd1da07e02
hro2009 <hrair5@inbox.ru>
2019-12-03 11:48:46 +0400

    files

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+    var $slider = $('.slider');
+
+    if ($slider.length) {
+        var currentSlide;
+        var slidesCount;
+        var sliderCounter = document.createElement('div');
+        sliderCounter.classList.add('slider__counter');
+
+        var updateSliderCounter = function(slick, currentIndex) {
+            currentSlide = slick.slickCurrentSlide() + 1;
+            slidesCount = slick.slideCount;
+            $(sliderCounter).text(currentSlide + ' of ' +slidesCount)
+        };
+
+        $slider.on('init', function(event, slick) {
+            $slider.append(sliderCounter);
+            updateSliderCounter(slick);
+        });
+
+        $slider.on('afterChange', function(event, slick, currentSlide) {
+            updateSliderCounter(slick, currentSlide);
+        });
+
+        $slider.slick({
+            autoplay:true,
+              responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+        });
+    }
+
+
+
+
+
+    $(".set > a").on("click", function() {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this)
+                .siblings(".acc-content")
+                .slideUp(200);
+            $(".set > a i")
+                .removeClass("fa-minus")
+                .addClass("fa-plus");
+        } else {
+            $(".set > a i")
+                .removeClass("fa-minus")
+                .addClass("fa-plus");
+            $(this)
+                .find("i")
+                .removeClass("fa-plus")
+                .addClass("fa-minus");
+            $(".set > a").removeClass("active");
+            $(this).addClass("active");
+            $(".acc-content").slideUp(200);
+            $(this)
+                .siblings(".acc-content")
+                .slideDown(200);
+        }
+    });
+
+
+
+
+
+});
+
+
